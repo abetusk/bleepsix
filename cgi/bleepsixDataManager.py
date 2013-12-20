590ce14e405118bc9aad943dbcb166a93dfd3d6d
abetusk <abetusk@gmail.com>
2013-12-20 00:35:38 -0500

    initial version of kicad sch download

--- cgi/bleepsixDataManager.py
+++ cgi/bleepsixDataManager.py
@@ -6,25 +6,42 @@ import cgitb
 import sys
 import json
 import uuid
+import subprocess as sp
 
 cgitb.enable();
 
+jsonsch_exec = "/tmp/pykicad/jsonsch.py"
 staging_base = "/tmp/stage"
 
 json_container = json.load(sys.stdin);
 msg_type = json_container["type"]
 
 u_id = uuid.uuid4()
-f = open( staging_base + "/" + str(u_id), "w" )
+sch_json_fn = staging_base + "/" + str(u_id)
+f = open( sch_json_fn, "w" )
 f.write( json.dumps(json_container["data"], indent=2) )
+#f.write( kicad_sch )
 f.close()
+
 
 print "Content-Type: application/json"
 print
 
-obj = { "type" : "id", "id" : str(u_id) }
-print json.dumps(obj)
+try:
+  kicad_sch = sp.check_output( [jsonsch_exec, sch_json_fn ] );
 
+  u_id = uuid.uuid4()
+  f = open( staging_base + "/" + str(u_id), "w" )
+  f.write( kicad_sch )
+  f.close()
+
+except Exception as e:
+  print "{ \"type\" : \"error\", \"message\" : \"" + str(e) + "\" }"
+  sys.exit(1)
+
+
+obj = { "type" : "id", "id" : str(u_id), "extra": "hmm.." + str(u_id)  }
+print json.dumps(obj)
 
 
 #print "Content-Type: text/html;charset=utf-8"
