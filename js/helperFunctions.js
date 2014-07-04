dd2b8232712a067cd5d0c3c9b6896340590d57a1
abetusk <abetusk@gmail.com>
2014-07-04 01:07:58 -0400

    updated license information in header (said 'Foobar', changed to 'bleepsix')

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -4,18 +4,18 @@
 
     This file is part of bleepsix v2.
 
-    Foobar is free software: you can redistribute it and/or modify
+    bleepsix is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    bleepsix is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with bleepsix.  If not, see <http://www.gnu.org/licenses/>.
 
     Parts based on bleepsix v1. BSD license
     by Rama Hoetzlein and Abram Connelly.
