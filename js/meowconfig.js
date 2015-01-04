b2b5cad91d527c2b5b3be0270a04b144d08dbe91
abetusk <abetusk@gmail.com>
2015-01-04 18:11:39 -0500

    disabling piwik in non-production mode. adding custom url for piwik footer.

--- js/meowconfig.js
+++ js/meowconfig.js
@@ -2,6 +2,10 @@
 
 /* DEVELOPMENT */
 var MEOWURL = "https://localhost";
+var MEOWBASEURL = "localhost";
+var PRODUCTION_FLAG = false;
 
 /* PRODUCTION */
 //var MEOWURL = "https://www.meowcad.com";
+//var MEOWBASEURL = "www.meowcad.com";
+//var PRODUCTION_FLAG = true;
